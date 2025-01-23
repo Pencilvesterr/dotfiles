3eadfc56d8a8b0183c26fc9d82057f90cfa97f7b
Morgan Crouch <mcrouch@atlassian.com>
2025-01-24 10:05:48 +1100

    Fix autosaving with auto-command

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Autosave when leaving the current buffer
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "VimLeavePre" }, {
+  group = vim.api.nvim_create_augroup("mcrouch:" .. "autosave", {}),
+  callback = function(event)
+    -- Check if the old buffer has been changed
+    if vim.bo[event.buf].modified then
+      vim.schedule(function()
+        vim.api.nvim_buf_call(event.buf, function()
+          -- Save the buffer
+          vim.cmd("silent! write")
+        end)
+      end)
+    end
+  end,
+})
