dd229ae40ce14c2e1e57f3d5ce16b296a3acfcf8
Morgan Crouch <mcrouch@atlassian.com>
2025-05-10 13:28:49 +0700

    Remove override for `sleep` command

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,7 +9,6 @@
 alias shutdown='sudo shutdown now'
 alias restart='sudo reboot'
 alias suspend='sudo pm-suspend'
-alias sleep='pmset sleepnow'
 alias c='clear'
 alias e='exit'
 alias vi='vim' # Mask builtin with better default
