a9e899c8028867d308326eb321a45fb39c727f63
Morgan Crouch <m8crouch@gmail.com>
2026-03-23 09:27:04 +1100

    Minor nvim config changes

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -2,9 +2,10 @@
 return {
   "jiaoshijie/undotree",
   dependencies = "nvim-lua/plenary.nvim",
+  enabled = false,
   config = true,
   keys = { -- load the plugin only when using it's keybinding:
     -- TODO: This key map isn't working yet...
-    { "<leader>uu", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle undo menu" },
+     -- { "<leader>uU", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle undo menu" },
   },
 }
