dbd0b38b67dc17c037d2b83fe98742da8a24803c
Morgan Crouch <m8crouch@gmail.com>
2026-03-29 08:33:05 +1100

    Add work devenv to ssh_domains in wezterm

--- wezterm/servers.lua
+++ wezterm/servers.lua
@@ -6,6 +6,16 @@ M.ssh_domains = {
 		remote_address = "192.168.1.244",
 		username = "morgan",
 	},
+	{
+		name = "devenv_work",
+		remote_address = "10.70.174.63",
+		username = "ubuntu",
+		ssh_option = {
+			identityfile = "~/.cache/atlassian/remote-dev-env/ssh/id_rsa_rde",
+			stricthostkeychecking = "no",
+			userknownhostsfile = "/dev/null",
+		},
+	},
 }
 
 return M
