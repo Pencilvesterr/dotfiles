d46f614c18113804fc29f7dc4e939962d9f34e27
Morgan Crouch <mcrouch@atlassian.com>
2026-06-20 09:53:25 +1000

    Cleanup nvim

--- nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
--- Yet to setup and use
--- Switch betweeen venvs in pythong
-return {
-  "AckslD/swenv.nvim",
-  enabled = false,
-}
--- nvim/lua/plugins/guess-indent.lua
+++ nvim/lua/plugins/guess-indent.lua
@@ -1,8 +1,8 @@
 -- Description: Plugin to guess the indent of a file
+-- https://github.com/NMAC427/guess-indent.nvim
 return {
   "nmac427/guess-indent.nvim",
   opts = {
     auto_cmd = true,
-    override_editorconfig = true,
   },
 }
