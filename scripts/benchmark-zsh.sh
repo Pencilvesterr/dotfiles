9f084c7f71395f11ff63b5f57660cabc1728af96
Morgan Crouch <m8crouch@gmail.com>
2026-07-11 16:05:33 +1000

    Minor benchmark script formatting imporvements

--- scripts/benchmark-zsh.sh
+++ scripts/benchmark-zsh.sh
@@ -17,12 +17,12 @@ echo "Running: time zsh -i -c exit (5 times)"
 
 total=0
 for i in {1..5}; do
-    result=$((time zsh -i -c exit) 2>&1 | grep real | awk '{print $2}' | sed 's/[ms]//g')
+    result=$( (time zsh -i -c exit) 2>&1 | grep real | awk '{print $2}' | sed 's/[ms]//g')
     echo "Run $i: ${result}s"
     # Convert to seconds for easier math (handle format like 0m0.765s)
     if [[ $result == *m* ]]; then
-        minutes=$(echo $result | cut -d'm' -f1)
-        seconds=$(echo $result | cut -d'm' -f2)
+        minutes=$(echo "$result" | cut -d'm' -f1)
+        seconds=$(echo "$result" | cut -d'm' -f2)
         result=$(echo "$minutes * 60 + $seconds" | bc -l 2>/dev/null || awk "BEGIN {print $minutes * 60 + $seconds}")
     fi
     total=$(echo "$total + $result" | bc -l 2>/dev/null || awk "BEGIN {print $total + $result}")
